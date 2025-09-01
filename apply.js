ad75df4e70488801976e497fae1b15994712a91d
Matildamathebula <161756130+Matildamathebula@users.noreply.github.com>
2025-09-02 00:18:32 +0200

    added a new function on JavaScript and updated app.css

--- apply.js
+++ apply.js
@@ -48,6 +48,11 @@ function handleSearchSubmit  (event) {
 
     searchCity(searchInput.value);
 }
+function formatDay(timestamp){
+    let date=new Date (timestamp * 1000);
+    let days=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days [date.getDay()];
+}
 
 function getForecast(city){
     let apiKey = "36029o83t7fb6b04f2aa5c604a48d52f";
@@ -59,20 +64,30 @@ function getForecast(city){
         console.log(response.data);
 
     let forecastElement=document.querySelector("#forecast");
-    let days=["Mon", "Tue", "Wed", "Thu", "Fri"];
+
     let forecastHtml="";
 
-    days.forEach(function(day){
-    forecastHtml=
-    forecastHtml+`
+    response.data.daily.forEach(function(day, index){
+        if (index < 5){
+
+    forecastHtml =
+      forecastHtml +
+      `
     <div class="weather-forecast-day">
-      <div class="weather-forecast-date">${day}</div>
-      <div class="weather-forecast-icon">⛈</div>
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      <img src="${
+        day.condition.icon_url
+      }" class="weather-forecast-icon" />
       <div class="weather-forecast-temperatures">
-        <div class="weather-forecast-temperature"><strong>15°</strong></div>
-        <div class="weather-forecast-temperature">35°</div>
+        <div class="weather-forecast-temperature"><strong>${Math.round(
+          day.temperature.maximum
+        )}°</strong></div>
+        <div class="weather-forecast-temperature">${Math.round(
+          day.temperature.minimum
+        )}°</div>
       </div>
     </div>`;
+        }
     });
     forecastElement.innerHTML=forecastHtml;
 }
